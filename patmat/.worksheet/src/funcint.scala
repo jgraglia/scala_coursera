55619c51eafd389e4530dbb5a91774d32fde7085
Julien Graglia <jgraglia@netceler.com>
2013-04-28 17:21:53 +0200

    videos

--- patmat/.worksheet/src/funcint.scala
+++ patmat/.worksheet/src/funcint.scala
@@ -51,5 +51,48 @@ assert(one == two - one );$skip(21);
 println((one + one));$skip(53); 
 assert(Zero == (one + one).predecessor.predecessor );$skip(17); 
 
-println("OKay")}
+println("OKay")
+
+
+trait Expr
+case class Number(x:Int) extends Expr;
+case class Sum(l:Expr, r:Expr) extends Expr;
+case class Prod(l:Expr, r:Expr) extends Expr;$skip(282); ;
+
+def eval(e:Expr):Int = e match{
+    case Number(x) => x
+    case Sum(l,r) => eval(l)+eval(r)
+    case Prod(l,r) => eval(l)*eval(r)
+    
+};System.out.println("""eval: (e: funcint.Expr)Int""");$skip(367); 
+def show(e:Expr):String = e match{
+    case Number(x) => x.toString()
+    case Sum(l,r) => show(l)+"+"+show(r)
+    case Prod(Number(a),Number(b)) => show(Number(a))+"*"+show(Number(b))
+    case Prod(Number(a),r) => show(Number(a))+"*("+show(r)+")"
+    case Prod(l,Number(a)) => "("+show(l)+")*"+show(Number(a))
+    case Prod(l,r) => "("+show(l)+") * ("+show(r)+")"
+};System.out.println("""show: (e: funcint.Expr)String""");$skip(51); 
+val e  =Prod(Sum(Number(1), Number(2)), Number(9));System.out.println("""e  : funcint.Prod = """ + $show(e ));$skip(20); val res$1 = 
+show(e)+"="+eval(e);System.out.println("""res1: String = """ + $show(res$1));$skip(32); val res$2 = 
+show(Sum(Number(1), Number(2)));System.out.println("""res2: String = """ + $show(res$2));$skip(48); val res$3 = 
+show(Sum(Number(1), Sum(Number(2), Number(3))));System.out.println("""res3: String = """ + $show(res$3))}
+
+/*
+def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+  case List() =>  List(x)
+  case y :: ys => {
+    if (ys.isEmpty()) y :: x
+    else y::  insert(x, ys.tail)
+	  /*if (x<y) List(y,  ys.head :: insert(x, ys.tail))
+	  else if (x == y) List(y, x, ys)
+	  else List(ys)
+	  */
+  }
+}
+insert(9, Nil)
+var a = 1::2::6::Nil
+insert(4, a)
+println(a.toString())
+*/
 }
