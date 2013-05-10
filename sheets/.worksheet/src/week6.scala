376dcfba4e8d020914fba73b68290d135672e517
Julien Graglia <jgraglia@netceler.com>
2013-05-10 20:54:13 +0200

    sentenceOccurrences

--- sheets/.worksheet/src/week6.scala
+++ sheets/.worksheet/src/week6.scala
@@ -1,21 +1,45 @@
-object week6 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(111); 
+object week6 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(32); 
+  val a ="dfgdfg";System.out.println("""a  : String = """ + $show(a ));$skip(20); val res$0 = 
+  a map (x=> (x,1));System.out.println("""res0: scala.collection.immutable.IndexedSeq[(Char, Int)] = """ + $show(res$0));$skip(28); 
+  val zz = List("ee", "dl");System.out.println("""zz  : List[String] = """ + $show(zz ));$skip(25); val res$1 = 
+  zz.flatten mkString "";System.out.println("""res1: String = """ + $show(res$1));$skip(100); 
+  
 	def isPrime(n: Int): Boolean = {
 	  var before =  2 until n
 	  before forall(i =>  n%i != 0)
-	};System.out.println("""isPrime: (n: Int)Boolean""");$skip(14); val res$0 = 
+	};System.out.println("""isPrime: (n: Int)Boolean""");$skip(14); val res$2 = 
 	
-	isPrime(1);System.out.println("""res0: Boolean = """ + $show(res$0));$skip(12); val res$1 = 
-	isPrime(2);System.out.println("""res1: Boolean = """ + $show(res$1));$skip(12); val res$2 = 
-	isPrime(4);System.out.println("""res2: Boolean = """ + $show(res$2));$skip(12); val res$3 = 
-	isPrime(7);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(13); val res$4 = 
-	isPrime(21);System.out.println("""res4: Boolean = """ + $show(res$4));$skip(157); 
+	isPrime(1);System.out.println("""res2: Boolean = """ + $show(res$2));$skip(12); val res$3 = 
+	isPrime(2);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(12); val res$4 = 
+	isPrime(4);System.out.println("""res4: Boolean = """ + $show(res$4));$skip(12); val res$5 = 
+	isPrime(7);System.out.println("""res5: Boolean = """ + $show(res$5));$skip(13); val res$6 = 
+	isPrime(21);System.out.println("""res6: Boolean = """ + $show(res$6));$skip(156); 
 	
   def scalarProduct(xs: List[Double], ys: List[Double]) : Double = {
     var z = for {
        i <- 0 until xs.size
-    } yield xs(i) *ys(i);
+    } yield xs(i) *ys(i)
     z sum
-  };System.out.println("""scalarProduct: (xs: List[Double], ys: List[Double])Double""");$skip(49); val res$5 = 
+  };System.out.println("""scalarProduct: (xs: List[Double], ys: List[Double])Double""");$skip(15); 
   
-  scalarProduct(List (1, 2, 4), List(1, 4,0));System.out.println("""res5: Double = """ + $show(res$5))}
+  var n = 4;System.out.println("""n  : Int = """ + $show(n ));$skip(46); val res$7 = 
+  scalarProduct(List (1, 2, 4), List(1, 4,0));System.out.println("""res7: Double = """ + $show(res$7));$skip(430); 
+  
+  def isSafe(col: Int, queens: List[Int]): Boolean = {
+    def leftDiag(): Boolean = {
+      val l = for (c <- col to 0 by -1; if queens contains c) yield true
+      l contains true
+    }
+    def rightDiag(): Boolean = {
+      val l = for (c <- col to n; if queens contains c) yield true
+      l contains true
+    }
+    if (queens contains col) false
+    else if (leftDiag) false
+    else if (rightDiag) false
+  	else true
+  };System.out.println("""isSafe: (col: Int, queens: List[Int])Boolean""");$skip(30); val res$8 = 
+  
+  isSafe(0, List(0, 1, 2));System.out.println("""res8: Boolean = """ + $show(res$8));$skip(21); val res$9 = 
+  isSafe(0, List(2));System.out.println("""res9: Boolean = """ + $show(res$9))}
 }
--- sheets/week6.sc
+++ sheets/week6.sc
@@ -1,21 +1,46 @@
 object week6 {
+  val a ="dfgdfg"                                 //> a  : String = dfgdfg
+  a map (x=> (x,1))                               //> res0: scala.collection.immutable.IndexedSeq[(Char, Int)] = Vector((d,1), (f,1
+                                                  //| ), (g,1), (d,1), (f,1), (g,1))
+  val zz = List("ee", "dl")                       //> zz  : List[String] = List(ee, dl)
+  zz.flatten mkString ""                          //> res1: String = eedl
+  
 	def isPrime(n: Int): Boolean = {
 	  var before =  2 until n
 	  before forall(i =>  n%i != 0)
 	}                                         //> isPrime: (n: Int)Boolean
 	
-	isPrime(1)                                //> res0: Boolean = true
-	isPrime(2)                                //> res1: Boolean = true
-	isPrime(4)                                //> res2: Boolean = false
-	isPrime(7)                                //> res3: Boolean = true
-	isPrime(21)                               //> res4: Boolean = false
+	isPrime(1)                                //> res2: Boolean = true
+	isPrime(2)                                //> res3: Boolean = true
+	isPrime(4)                                //> res4: Boolean = false
+	isPrime(7)                                //> res5: Boolean = true
+	isPrime(21)                               //> res6: Boolean = false
 	
   def scalarProduct(xs: List[Double], ys: List[Double]) : Double = {
     var z = for {
        i <- 0 until xs.size
-    } yield xs(i) *ys(i);
+    } yield xs(i) *ys(i)
     z sum
   }                                               //> scalarProduct: (xs: List[Double], ys: List[Double])Double
   
-  scalarProduct(List (1, 2, 4), List(1, 4,0))     //> res5: Double = 9.0
+  var n = 4                                       //> n  : Int = 4
+  scalarProduct(List (1, 2, 4), List(1, 4,0))     //> res7: Double = 9.0
+  
+  def isSafe(col: Int, queens: List[Int]): Boolean = {
+    def leftDiag(): Boolean = {
+      val l = for (c <- col to 0 by -1; if queens contains c) yield true
+      l contains true
+    }
+    def rightDiag(): Boolean = {
+      val l = for (c <- col to n; if queens contains c) yield true
+      l contains true
+    }
+    if (queens contains col) false
+    else if (leftDiag) false
+    else if (rightDiag) false
+  	else true
+  }                                               //> isSafe: (col: Int, queens: List[Int])Boolean
+  
+  isSafe(0, List(0, 1, 2))                        //> res8: Boolean = false
+  isSafe(0, List(2))                              //> res9: Boolean = false
 }
