062f5a2cc9ffabc9ad95daafc82d47e95d9ad440
Julien Graglia <jgraglia@netceler.com>
2013-05-10 19:14:26 +0200

    scalar

--- sheets/.worksheet/src/week6.scala
+++ sheets/.worksheet/src/week6.scala
@@ -8,7 +8,14 @@ object week6 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._;
 	isPrime(2);System.out.println("""res1: Boolean = """ + $show(res$1));$skip(12); val res$2 = 
 	isPrime(4);System.out.println("""res2: Boolean = """ + $show(res$2));$skip(12); val res$3 = 
 	isPrime(7);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(13); val res$4 = 
-	isPrime(21);System.out.println("""res4: Boolean = """ + $show(res$4));$skip(15); 
-	println (6%5);$skip(15); 
-	println (6%2)}
+	isPrime(21);System.out.println("""res4: Boolean = """ + $show(res$4));$skip(157); 
+	
+  def scalarProduct(xs: List[Double], ys: List[Double]) : Double = {
+    var z = for {
+       i <- 0 until xs.size
+    } yield xs(i) *ys(i);
+    z sum
+  };System.out.println("""scalarProduct: (xs: List[Double], ys: List[Double])Double""");$skip(49); val res$5 = 
+  
+  scalarProduct(List (1, 2, 4), List(1, 4,0));System.out.println("""res5: Double = """ + $show(res$5))}
 }
--- sheets/week6.sc
+++ sheets/week6.sc
@@ -9,5 +9,13 @@ object week6 {
 	isPrime(4)                                //> res2: Boolean = false
 	isPrime(7)                                //> res3: Boolean = true
 	isPrime(21)                               //> res4: Boolean = false
-                                           //> 0
+	
+  def scalarProduct(xs: List[Double], ys: List[Double]) : Double = {
+    var z = for {
+       i <- 0 until xs.size
+    } yield xs(i) *ys(i);
+    z sum
+  }                                               //> scalarProduct: (xs: List[Double], ys: List[Double])Double
+  
+  scalarProduct(List (1, 2, 4), List(1, 4,0))     //> res5: Double = 9.0
 }
